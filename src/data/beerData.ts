3e43af7493ddb7a1bcbc7262917f22262316e37a
Darcio Vilela <darcio@jp7.com.br>
2023-06-14 17:15:58 -0300

    Modify data format

--- src/data/beerData.ts
+++ src/data/beerData.ts
@@ -1,29 +1,34 @@
-import { Beer } from "../interfaces/beer";
+import { Item } from "../interfaces/beer"
 
-export const beerData:Beer[] = [
+export const beerData:Item[] = [
     {
+        id: 1,
         name: 'Corona',
         country: 'Mexico',
         style: 'Pale Lager'
     },
 
     {
+        id: 2,
         name: 'Guiness',
         country: 'Ireland',
         style: 'Stout'
     },
 
     {
+        id: 3,
         name: 'Heineken',
         country: 'Holland',
         style: 'Pale Lager'
     },
     {
+        id: 4,
         name: 'Tsingtao',
         country: 'China',
         style: 'Pale Lager'
     },
     {
+        id: 5,
         name: 'Red Stripe',
         country: 'Jamaica',
         style: 'Pale Amber Lager'
